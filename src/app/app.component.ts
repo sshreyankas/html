e3c02ffa1f2329dff5b186f88142ccf8594da792
shreyanka <shreyanka.suryawanshi@gmail.com>
2020-07-04 01:22:35 +0530

    1st commit;

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { title } from 'process';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'home';
+}
+export declare class MatCardModule {
+  constructor(parameters) {
+   
+  }
+}
+
